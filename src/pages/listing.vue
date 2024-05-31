9c48aa43660f329b0cbe21708bd5f1d46388a4d4
Attar74 <m.elattar.dev@gmail.com>
2024-05-31 03:21:38 +0300

    adding table listing

--- src/pages/listing.vue
+++ src/pages/listing.vue
@@ -2,15 +2,31 @@
   <div class="pa-8 w-100 bg-f4f3f9">
     <listingHeader />
     <listingItems class="my-6" />
+    <div class="d-flex justify-end">
+      <v-pagination
+        class="pagination"
+        circle
+        v-model="page"
+        :length="pageCount"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import listingHeader from '@/components/listing/listingHeader.vue';
+const page = 1;
+const pageCount = 4;
 </script>
 
 <style>
 .bg-f4f3f9 {
   background: #f4f3f9;
+}
+
+.v-pagination .v-pagination__list .v-pagination__item--is-active {
+  color: #fff;
+  background-color: #733ee4;
+  border-radius: 8px;
 }
 </style>
