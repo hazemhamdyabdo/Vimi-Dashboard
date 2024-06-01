81fa10f70ef4999d3fa5283fee03f627269a576e
Hazem.H <hazemhamdy117@gmail.com>
2024-06-01 21:35:45 +0300

    change listing => products

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../pages/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/auth/login',
       name: 'login',
       component: () => import('../pages/auth/loginView.vue'),
     },
@@ -28,17 +22,17 @@ const router = createRouter({
           },
         },
         {
-          path: 'listing',
-          name: 'listing',
-          component: () => import('@/pages/products/Listing.vue'),
+          path: 'products',
+          name: 'products',
+          component: () => import('@/pages/products/List.vue'),
           meta: {
-            title: 'listing',
+            title: 'products',
           },
         },
         {
           path: 'add-product',
           name: 'add-product',
-          component: () => import('@/pages/products/AddProducts.vue'),
+          component: () => import('@/pages/products/Add.vue'),
           meta: {
             title: 'Add Product',
           },
